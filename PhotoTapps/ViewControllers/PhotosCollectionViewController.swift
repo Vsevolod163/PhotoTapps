59ae3bcc4342fa54ebe90f753415eb70ebb5d2ab
Vsevolod <vsevolod.lashin@gmail.com>
2023-05-04 14:54:39 +0300

    sizeForItemAt, insetForSectionAt, minimumLineSpacingForSectionAt, minimumInteritemSpacingForSectionAt added

--- PhotoTapps/ViewControllers/PhotosCollectionViewController.swift
+++ PhotoTapps/ViewControllers/PhotosCollectionViewController.swift
@@ -11,7 +11,7 @@ final class PhotosCollectionViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        overrideUserInterfaceStyle = .light
     }
 
     // MARK: UICollectionViewDataSource
@@ -29,9 +29,29 @@ final class PhotosCollectionViewController: UICollectionViewController {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath) as! PhotoCell
         
         cell.backgroundColor = .black
-        
-        
-        
+    
         return cell
+    }
+}
+
+extension PhotosCollectionViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let itemsPerRow: CGFloat = 3
+        let paddingWidth = 20 * (itemsPerRow + 1)
+        let availableWidth = collectionView.frame.width - paddingWidth
+        let widthPerItem = availableWidth / itemsPerRow
+        return CGSize(width: widthPerItem, height: widthPerItem)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        20
     }
 }
